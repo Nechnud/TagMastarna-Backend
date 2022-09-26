dd5c52e7bc66b0e07b560a7a59fd68851506a959
Anton Olsson <antonolsson.imp@gmail.com>
2022-09-26 12:38:33 +0200

    Update Route.js

--- Classes/Route.js
+++ Classes/Route.js
@@ -1,3 +1,33 @@
+const http = require('http')
 module.exports = class Route{
+  stations = [];
+  constructor(/*ruttid*/routeId) {
+
+    this.routeID = routeId;
+    
+  }
+ getStationData(){
+  http.get(
+    `http://${process.env.API_HOST}:${process.env.PORT}${process.env.API_URL}/searchJourney/getStationForRoutes/?id=${this.routeID}`,
+    (resp) => {
+        let data = '';
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+          this.stations = JSON.parse(data);
+          console.log(this.stations)
+        });
+
+        resp.on('error', (err) => {
+            console.log(err.message);
+        });
+    }
+    
+   );
+   }
+    
   
+
 }
