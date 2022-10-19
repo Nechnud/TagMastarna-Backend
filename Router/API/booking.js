31a1d39b1f808adb7ea2701a61b5649ee54807b2
Anton Olsson <antonolsson.imp@gmail.com>
2022-10-19 13:31:25 +0200

    confirmationEmail feature

--- Router/API/booking.js
+++ Router/API/booking.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
 const {
-    postBookingAndTickets
+    postBookingAndTickets,
+    postConfirmationEmail
 } = require('../../Controller/Bookings');
 
 const routes = () => {
     const router = express.Router();
 
     router.post('/', postBookingAndTickets);
+    router.post('/confirmationEmail', postConfirmationEmail);
 
     return router;
 }
